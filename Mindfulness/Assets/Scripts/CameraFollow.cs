c87ca928466d9c4a7937a4c4e2ef55c77a5252be
DESKTOP-PEPNLPD\pawel <paweltruong@gmail.com>
2019-08-31 21:14:23 +0200

    ostatnie szlify i build

---
+++ Mindfulness/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform FollowTarget;
+    public Transform LeftMapEdge;
+    public Transform RightMapEdge;
+
+    public Transform LeftBoarder;
+    public Transform RightBoarder;
+    float correction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var distance = Vector3.Distance(RightBoarder.position, LeftBoarder.position);
+        correction = distance != 0 ? distance / 2 : 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(FollowTarget.position.x, LeftMapEdge.position.x + correction, RightMapEdge.position.x - correction), transform.position.y, transform.position.z);
+    }
+}
